6aed60f917ad8aa9597252413fdfe500aa3fbfe0
Ory Shvartz <oryshkat@gmail.com>
2020-12-29 18:32:16 +0200

    fixed try and except according to clarified instructions:

--- zip_job.py
+++ zip_job.py
@@ -5,10 +5,6 @@ arr = ['a', 'b', 'c', 'd']
 for i in arr:
     try:
         file = open(i + ".txt", 'w')
-    except FileNotFoundError:
-        # doesn't exist, create it
+    except IOError:
         print("file was not created, failing")
         sys.exit(1)
-    else:
-        # exists
-        print("File exists")
